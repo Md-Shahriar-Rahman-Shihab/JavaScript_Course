68c4fa4e077e94df8d2d3c37028cd58371b5a6d0
Shahriar Rahman Shihab <shihabshahriar21821@gmail.com>
2025-04-21 05:17:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "shihab@google.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+
+// accountId = 34563 // not allowed 
+
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
