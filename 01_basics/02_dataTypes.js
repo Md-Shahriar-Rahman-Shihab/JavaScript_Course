5a3ad01c967fab6e9f73e429cd8a3604cd1f7be5
Shahriar Rahman Shihab <shihabshahriar21821@gmail.com>
2025-04-22 19:04:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,44 @@
+"use strict"; //Treat all js code as newer version 
+
+// alert("Shihab"); //We are using NodeJS, not browser 
+
+console.log(3
+    +
+    3) // not use this 
+
+console.log(3+3); // code readability should be high
+
+console.log("Shihab")
+
+/*
+Documentation: 
+    1. mdn: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+    2. EcmaScript: https://tc39.es/ecma262/
+
+*/
+
+let name = "Shihab"
+let age = 24
+let isLoggedIn = true
+
+// number : 2 power 53 
+// bigint 
+// string => "" , ''
+// boolean => true/false
+// null => standalone value [ representation of empty value]
+// undefined => not defined [ variable declear but value not assined]
+// symbol => unique
+
+// object
+
+console.log(typeof "shihab"); // string
+console.log(typeof age); // number
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
+
